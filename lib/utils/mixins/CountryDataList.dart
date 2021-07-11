479b03e4aff71035487e3e43d7227ba44210353c
TucoBenedicto <w99snet@gmail.com>
2021-07-11 19:56:50 +0200

    ajouts de plusieurs fichiers

---
+++ lib/utils/mixins/CountryDataList.dart
@@ -0,0 +1,15 @@
+import 'CountryModelClass.dart';
+
+//class country {
+
+   List countrydata = [
+    InterestsModel(0, 'Japonais', 'assets/logo/bonsai1.jpg'),
+    InterestsModel(1, 'Senegalais', 'assets/logo/bonsai2.jpg'),
+    InterestsModel(2, 'Random', 'assets/logo/bonsai3.jpg'),
+    InterestsModel(3, 'American', 'assets/logo/bonsai4.jpg'),
+    InterestsModel(4, 'Marocan', 'assets/logo/bonsai5.jpg')
+  ];
+
+
+
+//}
--- lib/widgets/pageViewItem.dart
+++ lib/widgets/pageViewItem.dart
@@ -1,19 +1,33 @@
-
 // https://www.woolha.com/tutorials/flutter-creating-pageview-with-pagecontroller-examples
 // adb connect localhost:5555
 
 import 'package:flutter/material.dart';
+import '../utils/mixins/CountryModelClass.dart';
+import '../utils/mixins/CountryDataList.dart';
 
-class PageViewItem extends StatelessWidget{
+class PageViewItem extends StatelessWidget {
+
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 0);
 
-    return PageView(
+    print("Name : ${countrydata[2].imageAsset}");
+
+    final PageController controller =
+        PageController(initialPage: 1, keepPage: true, viewportFraction: 0.35);
+
+    return PageView.builder(
       /// [PageView.scrollDirection] defaults to [Axis.horizontal].
       /// Use [Axis.vertical] to scroll vertically.
       scrollDirection: Axis.horizontal,
       controller: controller,
-      children: const <Widget>[
+      itemCount: 5 /*interests.length*/,
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return Text(
+          '${'country.countrydata[1]'[index]}',
+        );
+      },
+      /*
+                const <Widget>[
         Center(
           child: Text('First Page'),
         ),
@@ -24,7 +38,8 @@ class PageViewItem extends StatelessWidget{
           child: Text('Third Page'),
         )
       ],
+           */
     );
+    //itembuilder()
   }
 }
-
